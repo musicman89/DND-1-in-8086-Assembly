ab28c613fd5632cc9886a2b09c2e4a9c910f9d8d
Davan Etelamaki <detelamaki@gmail.com>
2015-02-01 11:28:42 -0500

    Added Functions for Search, Cheat and Look Around

--- SourceFiles/Game/Game Loop/Actions/Look Around.asm
+++ SourceFiles/Game/Game Loop/Actions/Look Around.asm
@@ -6,27 +6,11 @@
 ;               void look_around();
 ;           Algorithm:
 ;               void look_around(){
-;					int yMin = 5;
-;					if(Character.y < yMin){
-;						yMin = Character.y;
-;					}
-;					int yMax = 5;
-;					else if(Character.y + yMax > 25){
-;						yMax = 25 - Character.y;
-;					}
-;					
-;					for(int y = Character.y - yMin; y <  Character.y + yMax; y++){
-;						int xMin = 5;
-;						if(Character.y < xMin){
-;							xMin = Character.y;
-;						}
-;						int xMax = 5;
-;						else if(Character.y + xMax > 25){
-;							xMax = 25 - Character.y;
-;						}
-;						int row = y * 25
-;						for(int x = Character.x - xMin; x < Character.x + xMax; x++){
-;								int tile = CurrentDungeon[row + x];
+;					get_y_bounds(5);
+;					get_x_bounds(5);
+;					for(int y = yMin; y <  yMax; y++){
+;						for(int x = xMin; x < xMax; x++){
+;								int tile = CurrentDungeon[rows[y] + x];
 ;								if(tile == 7 || tile == 8) tile = 9;
 ;								if(tile == 2) tile = 0;
 ;								if(tile == 3) tile = 1;
@@ -47,81 +31,49 @@
 ;       
 ;*******************************************************************************
 look_around:
-	mov cl, [Character + player.y]
-	mov ch, 10
-	cmp cl, 5
-	jl .lower_y
-	cmp cl, 20
-	jg .upper_y
-		sub cl, 5
-		jmp .starty
-	.upper_y:
-		mov ah, 25
-		sub ah, cl
-		sub ch, ah
-		sub cl, 5
-		jmp .starty
-	.lower_y:		
-		mov ah, 5
-		sub ah, cl
-		sub ch, ah
-		mov cl, 0
-		jmp .starty
-	.starty:
-		mov dl, [Character + player.x]
-		mov dh, 10
-		cmp dl, 5
-		jl .lower_x
-		cmp dl, 20
-		jg .upper_x
-			sub dl, 5
-			jmp .loopy
-		.upper_x:
-			mov ah, 25
-			sub ah, dl
-			sub dh, ah
-			sub dl, 5
-			jmp .loopy
-		.lower_x:		
-			mov ah, 5
-			sub ah, dl
-			sub dh, ah
-			mov dl, 0
-			jmp .loopy
-		.loopy:
-			push dx
-			mov ah, 0
+	mov ch, 5
+	call get_y_bounds
+
+	mov dh, 5
+	call get_x_bounds
+	.loopy:
+		push dx
+		.loopx:
+			mov bh, 0
+
+			add bl, cl
+			shl bx, 1
+			mov bx, [rows + bx]
+
 			mov al, cl
-			mov bx, 25
-			mul bx
-			.loopx:
-				add bh, 0
-				add bl, dl
-				add bx, ax
-				mov bx, [CurrentDungeon + bx]
-				cmp bx, 7
-				jne .not7
-					mov bx, 9
-				.not7:
-				cmp bx, 8
-				jne .not8
-					mov bx, 9
-				.not8:
-				cmp bx, 2
-				jne .not2
-					mov bx, 0
-				.not2:
-				cmp bx, 3
-				jne .not3
-					mov bx, 1
-				.not3:
-				call print_dec
-				inc dl
-				dec dh
-				jnz .loopx
-			call new_line
-			inc cl
-			dec ch
-			jnz .loopy
+			mov ah, 0
+			add bx, ax
+
+			mov bx, [CurrentDungeon + bx]
+			cmp bx, 7
+			jne .not7
+				mov bx, 9
+			.not7:
+			cmp bx, 8
+			jne .not8
+				mov bx, 9
+			.not8:
+			cmp bx, 2
+			jne .not2
+				mov bx, 0
+			.not2:
+			cmp bx, 3
+			jne .not3
+				mov bx, 1
+			.not3:
+			call print_dec
+			inc dl
+			cmp dl, dh
+			jl .loopx
+			jl .loopx
+	pop dx
+	inc cl
+	cmp cl, ch
+	jl .loopy
 	call pass
 ret
--- SourceFiles/Game/Game Loop/Player.asm
+++ SourceFiles/Game/Game Loop/Player.asm
@@ -198,3 +198,154 @@ show_hp:
 	call print_dec
 	call new_line
 ret
+
+;********************************************************************************
+;   get_y_bounds
+;   Purpose:
+;      To limit the bounds to the vertical range of the map
+;           Prototype:
+;               Void get_y_bounds(byte range);
+;           Algorithm:
+;               Void get_y_bounds(byte range){
+;					int starty = Character.y;
+;					int endy = 0;
+;					if(starty - range > 0){
+;						if(starty + range < 25){
+;							starty -= range;
+;							endy = starty + range * 2;
+;						}
+;						else{
+;							starty = (starty - range) ;
+;							endy = 25;
+;						}
+;					}
+;					else{
+;						endy = (starty + range) ;
+;						starty = 0;
+;					}
+;               }
+;               
+;   Entry:
+;       Byte Range in CH
+;   Exit:
+;       starty in CL, endy in CH
+;   Uses:
+;       AX, BX, CX
+;   Exceptions:
+;       
+;*******************************************************************************
+get_y_bounds:
+	mov cl, [Character + player.y]
+	mov bl, cl
+	sub bl, ch
+	cmp bl, 0
+	jl .lower_x
+	mov bl, cl
+	add bl, ch
+	cmp bl, 25
+	jg .upper_x
+		sub ch, ch
+		mov bl, ch
+		mov ch, cl
+		add ch, bl
+		add ch, bl
+		jmp .return
+	.upper_x:
+		sub cl, ch
+		mov ch, 25
+		jmp .return
+	.lower_x:		
+		add ch, cl
+		mov cl, 0
+		jmp .return
+	.return:
+ret
+
+;********************************************************************************
+;   get_x_bounds
+;   Purpose:
+;      To limit the bounds to the horizongal range of the map
+;           Prototype:
+;               Void get_x_bounds(byte range);
+;           Algorithm:
+;               Void get_x_bounds(byte range){
+;					int startx = Character.x;
+;					int endx = 0;
+;					if(startx - range > 0){
+;						if(startx + range < 25){
+;							startx -= range;
+;							endx = startx + range * 2;
+;						}
+;						else{
+;							startx = startx - range;
+;							endx = 25;
+;						}
+;					}
+;					else{
+;						endx = startx + range;
+;						startx = 0;
+;					}
+;               }
+;               
+;   Entry:
+;       Byte Range in DH
+;   Exit:
+;       startx in DL, endx in DH
+;   Uses:
+;       BX, DX
+;   Exceptions:
+;       
+;*******************************************************************************
+get_x_bounds:
+	mov dl, [Character + player.x]
+	mov bl, dl
+	sub bl, dh
+	cmp bl, 0
+	jl .lower_x
+	mov bl, dl
+	add bl, dh
+	cmp bl, 25
+	jg .upper_x
+		sub dl, dh
+		mov bl, dh
+		mov dh, dl
+		add dh, bl
+		add dh, bl
+		jmp .return
+	.upper_x:
+		sub dl, dh
+		mov dh, 25
+		jmp .return
+	.lower_x:		
+		add dh, dl
+		mov dl, 0
+		jmp .return
+	.return:
+ret
+
+rows:
+dw 25 * 0
+dw 25 * 1
+dw 25 * 2
+dw 25 * 3
+dw 25 * 4
+dw 25 * 5
+dw 25 * 6
+dw 25 * 7
+dw 25 * 8
+dw 25 * 9
+dw 25 * 10
+dw 25 * 11
+dw 25 * 12
+dw 25 * 13
+dw 25 * 14
+dw 25 * 15
+dw 25 * 16
+dw 25 * 17
+dw 25 * 18
+dw 25 * 19
+dw 25 * 20
+dw 25 * 21
+dw 25 * 22
+dw 25 * 23
+dw 25 * 24
